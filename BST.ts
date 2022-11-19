e696baefc669cbeacea9ddc99a57d69363970431
Toby Huynh <tobymhuynh0224@gmail.com>
2022-11-18 19:33:33 -0500

    First commit

---
+++ BST.ts
@@ -0,0 +1,11 @@
+class TreeNode {
+    data: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+
+    constructor(data: number) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
---
+++ out/BST.js
@@ -0,0 +1,9 @@
+var TreeNode = /** @class */ (function () {
+    function TreeNode(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    return TreeNode;
+}());
+//# sourceMappingURL=BST.js.map
